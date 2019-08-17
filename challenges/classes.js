94dc69b40b5704fd3aebbe3140138816b2674ecd
Paul M Edwards <pauledwards@gmail.com>
2019-08-16 20:44:10 -0700

    Rename cuboid to cuboid2 due to conflict

--- challenges/classes.js
+++ challenges/classes.js
@@ -49,17 +49,17 @@ class CuboidMaker2 {
   Create a cuboid object that uses the new keyword to use our CuboidMaker constructor
   Add properties and values of length: 4, width: 5, and height: 5 to cuboid.   
 */
-const cuboid = new CuboidMaker2({
+const cuboid2 = new CuboidMaker2({
   length: 4,
   width: 5,
   height: 5
 });
 
-console.log(cuboid);
+console.log(cuboid2);
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-console.log(cuboid.volume()); // 100
-console.log(cuboid.surfaceArea()); // 130
+console.log(cuboid2.volume()); // 100
+console.log(cuboid2.surfaceArea()); // 130
 
 console.log("");  //blank line
 
